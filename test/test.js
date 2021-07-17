76022c735e4a9f80b8ea728d68f70576e7660e89
chukwuemeke obi <fanzyboo@gmail.com>
2021-07-17 13:54:09 +0100

    Setup CICD

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
     const app = require("../index");
 
     describe("GET /", () => {
-      it("respond with Hello World", (done) => {
-        request(app).get("/").expect("Hello World", done);
+      it("Hello Ogba Education Clinic - OEC", (done) => {
+        request(app).get("/").expect("Hello Ogba Education Clinic - OEC", done);
       })
     });
